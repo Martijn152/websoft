698804811d363699eda926cadcf9ebe509be6d3f
Martijn152 <m.dered@hotmail.com>
2020-02-09 21:26:01 +0100

    Added schools.html
    - Added a local json file called schools.json
    - main.js fetches from that file

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -3,8 +3,58 @@
  */
 (function () {
     "use strict";
+    fillTable();
 
-
-
-    console.log("All ready.");
 }());
+
+function fillTable() {
+
+    const table = document.getElementById('table');
+    const url = 'schools.json';
+
+    fetch(url)
+        .then((data) => data.json())
+        .then(function (data) {
+
+            //Prepping the data for use
+            data = JSON.stringify(data);
+            data = JSON.parse(data);
+
+            //Creating the column names
+            var keys = Object.keys(data.Skolenheter[0]);
+            var thead = table.createTHead();
+            var headRow = thead.insertRow();
+            keys.forEach(function (key) {
+                var th = document.createElement("th");
+                var text = document.createTextNode(key);
+                th.appendChild(text);
+                headRow.appendChild(th);
+            });
+
+            //Logging length of the data set
+            console.log("Columns in Skolenheter: " + data.Skolenheter.length);
+
+            //Add new row for each school
+            data.Skolenheter.forEach(function(element){
+               var row = table.insertRow();
+
+               //Get keys to be able to iterate through the elements for the row
+               var elementKeys = Object.keys(element);
+
+               //Add new cell for each element in the row
+               elementKeys.forEach(function (elementCell) {
+                   var cell = row.insertCell();
+                   cell.append(document.createTextNode(element[elementCell]));
+               });
+            });
+
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+
+}
+
+	
