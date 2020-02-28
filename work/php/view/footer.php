46b3d8b515d7fb976d06446df7eca8c9803d84ce
Martijn152 <m.dered@hotmail.com>
2020-02-28 19:33:35 +0100

    Repaired the broken duckie

--- work/php/view/footer.php
+++ work/php/view/footer.php
@@ -1,7 +1,7 @@
 <footer>
-<hr>
-<p>An incredibly nice footer.</p>
-<div id='duck' style="height: 100px; width: 100px; position: absolute"></div>
-<script type="text/javascript" src="/js/duckie.js"></script>
-<h3 id="counterElement">Duckies caught: 0</h3>
+    <hr>
+    <p>An incredibly nice footer.</p>
+    <div id='duck' style="height: 100px; width: 100px; position: absolute"></div>
+    <h3 id="counterElement">Duckies caught: 0</h3>
+    <script type="text/javascript" src="/js/duckie.js"></script>
 </footer>
